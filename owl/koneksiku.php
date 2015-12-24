871944dae6c3ac2ff4d5cc99124d73f4ca07ab6d
gundambison <gundambison@gmail.com>
2015-12-24 10:34:05 +0700

    koneksi
    berikan check koneksi.. apakah **valid** atau tidak

--- owl/koneksiku.php
+++ owl/koneksiku.php
@@ -4,4 +4,11 @@
 	$password = '';
 	$database = 'databuku';
 	$koneksi = mysqli_connect($server, $username, $password, $database);
+//==================LENGKAPILAH dengan ini
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
+
+	
 ?>
